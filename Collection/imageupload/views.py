b0c21cb76e42296b296016bff08dd8d46ad50b56
Ajay Kumar Bharti <ajay.ajaykumar.kumar494@gmail.com>
2020-07-29 10:08:39 +0530

    Update Imageupload: Now list of images is showing after upload

--- Collection/imageupload/views.py
+++ Collection/imageupload/views.py
@@ -3,12 +3,14 @@ from .models import UploadImage
 from .forms import  UploadForm
 
 def index(request):
+    images = UploadImage.objects.all()
+    print(images)
     form = UploadForm()
     if request.method == 'POST':
         form = UploadForm(request.POST,request.FILES)
         if form.is_valid():
             form.save()
-    context = {'form':form}
+    context = {'form':form,'images':images}
     return render(request,'imageupload/upload.html',context)
 
 # Create your views here.
